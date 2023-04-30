82d3006fdf2e1d5909a77d734438588c3534000e
Roger102090 <130093686+Roger102090@users.noreply.github.com>
2023-04-30 09:24:55 +0000

    Calculadora de imc

---
+++ proyect1/scripts/script.js
@@ -0,0 +1,34 @@
+/*Seleccionar elementos del html*/
+const peso = document.querySelector('#kilo');
+const altura = document.querySelector('#metros');
+const form = document.querySelector('#form');
+const res_form = document.querySelector('#resultado')
+console.log(peso);
+console.log(altura);
+console.log(res_form);
+
+let p;
+let a; 
+
+
+form.addEventListener('submit', function res(event) {
+    event.preventDefault();
+    p = peso.value;    
+    a = altura.value;    
+    imc1 = a*a;
+    imc2 = p/imc1;
+    if ((isNaN(p) || !isFinite(p)) || (isNaN(a) || !isFinite(a))) {
+        alert ('Debes de ungresar un valor numérico');
+    }else {
+        a = parseInt(altura);
+        p = parseInt(peso);
+        const resultado = document.createElement('P');
+        resultado.textContent = imc2;
+        res_form.appendChild(resultado);
+    }
+   
+} )
+
+
+
+
