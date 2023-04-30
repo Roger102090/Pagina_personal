4d83d76f7280d7df8c88346d339dd57ecb9fa47d
Roger102090 <130093686+Roger102090@users.noreply.github.com>
2023-04-30 13:29:46 +0000

    Corrección de la calculadora

--- proyect1/scripts/script.js
+++ proyect1/scripts/script.js
@@ -9,6 +9,12 @@ console.log(res_form);
 
 let p;
 let a; 
+let n;
+let n1;
+const resultado = document.createElement('P');
+resultado.classList.add('reslutado__submit');
+resultado.textContent = 0;
+res_form.appendChild(resultado);
 
 
 form.addEventListener('submit', function res(event) {
@@ -17,16 +23,32 @@ form.addEventListener('submit', function res(event) {
     a = altura.value;    
     imc1 = a*a;
     imc2 = p/imc1;
+    let verif = document.querySelector('#resultado');
+    let verif1 = verif.value; 
+    verif1 = parseInt(verif);
+    if  (verif1 == "" || verif1 == 0) {
+        res_form.removeChild(resultado);
+    }else {   
+        
     if ((isNaN(p) || !isFinite(p)) || (isNaN(a) || !isFinite(a))) {
         alert ('Debes de ungresar un valor numérico');
     }else {
         a = parseInt(altura);
         p = parseInt(peso);
-        const resultado = document.createElement('P');
         resultado.textContent = imc2;
-        resultado.classList.add ('reslutado__submit')
-        res_form.appendChild(resultado);
-    }
+        resultado.classList.add('reslutado__submit');
+        res_form.appendChild(resultado);      
+        
+        // n = imc2;
+        // n1 = p;
+        // while (n <= 25) {
+        //     n = n1/imc1;
+        //     n1 = n1-.5;
+        // };
+        // const advice = document.createElement('P');
+        // advice.textContent = 'El resultado del calculo fue de:<br>', imc2, '<br>por lo que es recomendable que se tenga un peso de:<br>', n1, 
+        //pendiente
+    }}
    
 } )
 
